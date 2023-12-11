79d9825758b881f3a7143716a8a4e9392b907c07
noraammann <29450024-noraammann@users.noreply.replit.com>
2023-12-11 04:06:53 +0000

    added some db updates with jacob

--- main.ts
+++ main.ts
@@ -1,7 +1,69 @@
 import axios from 'axios';
 import moment from 'moment';
+import { Client } from 'pg';
+
+const client = new Client({
+  connectionString: process.env['DATABASE_URL'],
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect(err => {
+  if (err) {
+    console.error('Failed to connect to the database!', err.stack);
+  } else {
+    console.log('Successfully connected to the database.');
+    // Here you can proceed to execute queries on the database
+  }
+});
+
+
+// const trackedQuestions = [
+//   "https://manifold.markets/MichaelWheatley/who-first-builds-an-artificial-gene",
+//   "https://manifold.markets/Ledger/will-china-be-competitive-in-the-ll",
+//   "https://manifold.markets/ZviMowshowitz/will-google-have-the-best-llm-by-eo-b4ad29f8b98d",
+//   "https://manifold.markets/MatthewBarnett/will-a-machine-learning-training-ru-1fc7686995d5",
+//   "https://manifold.markets/MatthewBarnett/will-an-entity-purchase-at-least-10",
+//   "https://manifold.markets/MatthewBarnett/will-an-entity-be-confirmed-to-have",
+//   "https://manifold.markets/MatthewBarnett/will-nvidias-market-capitalization",
+//   "https://manifold.markets/MatthewBarnett/will-the-world-economy-grow-by-more-990c1df2ab63",
+//   "https://manifold.markets/MatthewBarnett/will-any-top-ai-lab-commit-to-a-mor",
+//   "https://manifold.markets/Duncn/will-humans-deliberately-cause-an-a",
+//   "https://manifold.markets/BestGuess/will-there-be-a-global-pause-on-cut",
+//   "https://manifold.markets/SimeonCampos/will-openai-pause-capabilities-rd-b",
+//   "https://manifold.markets/CalebW/in-2030-will-we-think-flis-6-month",
+//   "https://manifold.markets/MatthewBarnett/will-we-have-nearcomplete-automatio",
+//   "https://manifold.markets/MatthewBarnett/will-ais-be-widely-recognized-as-ha-6d69b9aa5a3a",
+//   "https://manifold.markets/NoaNabeshima/whole-brain-emulation-will-come-bef",
+//   "https://manifold.markets/logaems/human-whole-brain-emulation-before",
+//   "https://manifold.markets/rotatingpaguro/whole-primate-brain-connectome-by-2",
+//   "https://manifold.markets/SneakySly/will-llm-hallucinations-be-a-fixed-0f4541372303",
+//   "https://manifold.markets/vluzko/will-there-be-an-advance-in-llms-co",
+//   "https://manifold.markets/PeterWildeford/will-an-llm-have-been-reported-to-e",
+//   "https://manifold.markets/Austin/will-an-ai-get-gold-on-any-internat",
+//   "https://manifold.markets/EliezerYudkowsky/is-lecun-right-that-opensource-ai-w",
+//   "https://manifold.markets/EliezerYudkowsky/will-artificial-superintelligence-e",
+//   "https://manifold.markets/IsaacKing/by-the-end-of-2028-will-there-be-a-9a7a1b6d31a7",
+//   "https://manifold.markets/MartinRandall/will-ai-wipe-out-humanity-before-th-d8733b2114a8",
+//   "https://manifold.markets/EliezerYudkowsky/will-ai-wipe-out-humanity-by-2030-r",
+//   "https://manifold.markets/EliezerYudkowsky/if-artificial-general-intelligence",
+//   "https://manifold.markets/EliezerYudkowsky/will-hamas-carry-out-at-least-3-let",
+//   "https://manifold.markets/levifinkelstein/will-we-find-out-in-2023-about-a-na-0818ad690161",
+//   "https://manifold.markets/ZviMowshowitz/will-ai-write-75-of-social-media-vi"
+// ]
+
 
 const SLACK_ON = true;
+
+interface TrackedMarket {
+  _id: string, 
+  url: string, 
+  lastslacktime?: Date,
+  lastslackhourwindow?: number,
+  tracked: boolean,
+  last_report_sent?: string
+}
 
 interface Bet {
   createdTime: string;
@@ -154,39 +216,23 @@ interface BinaryMarketWithProbChanges extends BinaryMarket {
   };
 }
 
-const trackedQuestions = [
-  "https://manifold.markets/MichaelWheatley/who-first-builds-an-artificial-gene",
-  "https://manifold.markets/Ledger/will-china-be-competitive-in-the-ll",
-  "https://manifold.markets/ZviMowshowitz/will-google-have-the-best-llm-by-eo-b4ad29f8b98d",
-  "https://manifold.markets/MatthewBarnett/will-a-machine-learning-training-ru-1fc7686995d5",
-  "https://manifold.markets/MatthewBarnett/will-an-entity-purchase-at-least-10",
-  "https://manifold.markets/MatthewBarnett/will-an-entity-be-confirmed-to-have",
-  "https://manifold.markets/MatthewBarnett/will-nvidias-market-capitalization",
-  "https://manifold.markets/MatthewBarnett/will-the-world-economy-grow-by-more-990c1df2ab63",
-  "https://manifold.markets/MatthewBarnett/will-any-top-ai-lab-commit-to-a-mor",
-  "https://manifold.markets/Duncn/will-humans-deliberately-cause-an-a",
-  "https://manifold.markets/BestGuess/will-there-be-a-global-pause-on-cut",
-  "https://manifold.markets/SimeonCampos/will-openai-pause-capabilities-rd-b",
-  "https://manifold.markets/CalebW/in-2030-will-we-think-flis-6-month",
-  "https://manifold.markets/MatthewBarnett/will-we-have-nearcomplete-automatio",
-  "https://manifold.markets/MatthewBarnett/will-ais-be-widely-recognized-as-ha-6d69b9aa5a3a",
-  "https://manifold.markets/NoaNabeshima/whole-brain-emulation-will-come-bef",
-  "https://manifold.markets/logaems/human-whole-brain-emulation-before",
-  "https://manifold.markets/rotatingpaguro/whole-primate-brain-connectome-by-2",
-  "https://manifold.markets/SneakySly/will-llm-hallucinations-be-a-fixed-0f4541372303",
-  "https://manifold.markets/vluzko/will-there-be-an-advance-in-llms-co",
-  "https://manifold.markets/PeterWildeford/will-an-llm-have-been-reported-to-e",
-  "https://manifold.markets/Austin/will-an-ai-get-gold-on-any-internat",
-  "https://manifold.markets/EliezerYudkowsky/is-lecun-right-that-opensource-ai-w",
-  "https://manifold.markets/EliezerYudkowsky/will-artificial-superintelligence-e",
-  "https://manifold.markets/IsaacKing/by-the-end-of-2028-will-there-be-a-9a7a1b6d31a7",
-  "https://manifold.markets/MartinRandall/will-ai-wipe-out-humanity-before-th-d8733b2114a8",
-  "https://manifold.markets/EliezerYudkowsky/will-ai-wipe-out-humanity-by-2030-r",
-  "https://manifold.markets/EliezerYudkowsky/if-artificial-general-intelligence",
-  "https://manifold.markets/EliezerYudkowsky/will-hamas-carry-out-at-least-3-let",
-  "https://manifold.markets/levifinkelstein/will-we-find-out-in-2023-about-a-na-0818ad690161",
-  "https://manifold.markets/ZviMowshowitz/will-ai-write-75-of-social-media-vi"
-]
+const fetchTrackedQuestions = async (): Promise<TrackedMarket[]> => {
+  const queryText = 'SELECT _id, url, lastslacktime, lastslackhourwindow, tracked FROM markets WHERE tracked = true';
+  try {
+    const res = await client.query(queryText);
+    const trackedMarkets: TrackedMarket[] = res.rows.map(row => ({
+      _id: row._id,
+      url: row.url,
+      lastslacktime: row.lastslacktime,
+      lastslackhourwindow: row.lastslackhourwindow,
+      tracked: row.tracked
+    }));
+    return trackedMarkets;
+  } catch (error) {
+    console.error('Error fetching tracked questions', error);
+    throw error;
+  }
+};
 
 const getJsonUrl = (url: string): string => {
   const urlObj = new URL(url);
@@ -237,7 +283,21 @@ const getBets = async (marketId: string): Promise<Bet[]> => {
   }
 };
 
-const sendSlackMessage = async (url: string, marketName: string, marketId: string, report: string, comments?:string): Promise<void> => {
+const updateLastSlackInfo = async (url: string, timeWindow: number): Promise<void> => {
+  const queryText = `
+    UPDATE markets
+    SET lastSlackTime = NOW(), lastSlackHourWindow = $1
+    WHERE url = $2
+  `;
+  try {
+    await client.query(queryText, [timeWindow, url]);
+    console.log('Updated lastSlackTime and lastSlackHourWindow in the database.');
+  } catch (error) {
+    console.error('Error updating lastSlackTime and lastSlackHourWindow in the database:', error);
+  }
+};
+
+const sendSlackMessage = async (url: string, marketName: string, marketId: string, report: string, comments?:string, timeWindow?: number): Promise<void> => {
   const payload = {
     url,
     market_name: marketName,
@@ -247,11 +307,15 @@ const sendSlackMessage = async (url: string, marketName: string, marketId: strin
   };
 
   try {
-    await axios.post('https://hooks.slack.com/triggers/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', payload, {
+    const response = await axios.post('https://hooks.slack.com/triggers/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', payload, {
       headers: {
         'Content-Type': 'application/json',
       },
     });
+    if (response.status === 200 && timeWindow) {
+      // Slack message sent successfully, update database
+      await updateLastSlackInfo(url, timeWindow);
+    }
   } catch (error) {
     console.error(`Error occurred while sending Slack message: ${error}`);
   }
@@ -298,18 +362,20 @@ const getCommentsNote = async (marketId: string, t: number): Promise<string> => 
   return report;
 };
 
-const getChangeReport = async (market: Market): Promise<{reportWorthy: boolean, changeNote: string, commentsNote: string}> => {
+// TODO: move commentTime and timeWindow into same variable? 
+const getChangeReport = async (market: Market): Promise<{reportWorthy: boolean, changeNote: string, commentsNote: string, timeWindow: number}> => {
   let reportWorthy = false;
   let changeNote = '';
   let commentsNote = '';
-
+  let timeWindow = 24;
   let commentTime = 72; // default value
+  const delta = 0.02
   
   const getDirectionAndNote = (change: number, period: string): { direction: string, changeNote: string, time: number } => {
     let direction = '';
     let changeNote = '';
     let time = 72;
-    if (change > 0.1) {
+    if (change > delta) {
       direction = change > 0 ? ':chart_with_upwards_trend: Up' : ':chart_with_downwards_trend: Down';
       changeNote = `${direction} ${formatProb(change)}% in the last ${period}`;
       time = period === 'day' ? 24 : period === 'week' ? 24 * 7 : 24 * 30;
@@ -326,7 +392,7 @@ const getChangeReport = async (market: Market): Promise<{reportWorthy: boolean, 
 
     const marketWithChanges: BinaryMarketWithProbChanges = { ...market, probChanges };
     
-    if (probChanges.day > 0.1 || probChanges.week > 0.1 || probChanges.month > 0.1) {
+    if (probChanges.day > delta || probChanges.week > delta || probChanges.month > delta) {
       reportWorthy = true;
       let { changeNote: dayNote, time: dayTime } = getDirectionAndNote(probChanges.day, 'day');
       let { changeNote: weekNote, time: weekTime } = getDirectionAndNote(probChanges.week, 'week');
@@ -349,7 +415,7 @@ const getChangeReport = async (market: Market): Promise<{reportWorthy: boolean, 
         }
       }
       
-      if (probChanges.day > 0.1 || probChanges.week > 0.1 || probChanges.month > 0.1) {
+      if (probChanges.day > delta || probChanges.week > delta || probChanges.month > delta) {
         reportWorthy = true;
         let { changeNote: dayNote, time: dayTime } = getDirectionAndNote(probChanges.day, 'day');
         let { changeNote: weekNote, time: weekTime } = getDirectionAndNote(probChanges.week, 'week');
@@ -367,20 +433,57 @@ const getChangeReport = async (market: Market): Promise<{reportWorthy: boolean, 
     commentsNote = await getCommentsNote(market.id, commentTime);
   }
 
-  return { reportWorthy, changeNote, commentsNote };
+  return { reportWorthy, changeNote, commentsNote, timeWindow };
 };
 
-const checkAndSendUpdates = async (): Promise<void> => {
-  const markets = await Promise.all(trackedQuestions.map(url => getMarket(getJsonUrl(url))));
+const updateLocalMarket = async (id: string, lastslacktime: Date, lastslackhourwindow: number, last_report_sent: string): Promise<void> => {
+  const queryText = `
+    UPDATE markets
+    SET lastslacktime = $1, lastslackhourwindow = $2, last_report_sent = $3
+    WHERE _id = $4
+    `
+  try {
+    await client.query(queryText, [lastslacktime, lastslackhourwindow, last_report_sent, id]);
+    console.log('Local market updated in the database.');
+  } catch (error) {
+    console.error('Error updating local market in the database:', error);
+  }
+};
+
+console.log("hi")
+
+const checkAndSendUpdates = async (localMarkets:TrackedMarket[]): Promise<void> => {
+  const fetchedMarkets = await Promise.all(localMarkets.map(q => getMarket(getJsonUrl(q.url)))); // todo: this is silly, fix
   
-  for (const market of markets) {
-    const { reportWorthy, changeNote, commentsNote } = await getChangeReport(market);
-    console.log(market.url, "Send report: ", reportWorthy, changeNote)
+  for (const fetchedMarket of fetchedMarkets) {
+    const localMarket = localMarkets.find(q => q.url === fetchedMarket.url);
+    const { reportWorthy, changeNote, commentsNote, timeWindow } = await getChangeReport(fetchedMarket);
+    const isTimeForNewUpdate = !!localMarket && (!localMarket.lastslacktime ? true : ((Date.now() - new Date(localMarket.lastslacktime).getTime()) > (timeWindow * 60 * 60 * 1000))) 
+    const toSendReport = (reportWorthy && SLACK_ON && isTimeForNewUpdate)
     
-    if (reportWorthy && SLACK_ON) {
-      await sendSlackMessage(market.url, (market.outcomeType === "BINARY" ? `(${formatProb(market.probability)}%) ` : "") + market.question, market.id, changeNote, commentsNote);
+    console.log("Send report? ", toSendReport, changeNote, fetchedMarket.url)
+
+    if (toSendReport) {
+      const marketName = (fetchedMarket.outcomeType === "BINARY" ? `(${formatProb(fetchedMarket.probability)}%) ` : "") + fetchedMarket.question
+      await sendSlackMessage(fetchedMarket.url, marketName, fetchedMarket.id, changeNote, commentsNote);
+      updateLocalMarket(localMarket._id, new Date(), timeWindow, changeNote) 
     } 
   }
 };
 
-checkAndSendUpdates();
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const loop = async () => {
+  while (true) {
+    try {
+      const questions = await fetchTrackedQuestions();
+      await checkAndSendUpdates(questions);
+    } catch (error) {
+      console.error(error);
+    }
+    await sleep(60 * 60 * 1000); // 10 seconds delay before proceeding to the next iteration of the loop
+  }
+};
+
+loop(); // assume takes 1 second to run
+
