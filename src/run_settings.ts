d59138fe2835b139cafef15c511e00e4caf871ee
lagerros <jacobklagerros@gmail.com>
2023-12-16 06:05:23 +0000

    slack on. (should this always be hte case for deploys?)

--- src/run_settings.ts
+++ src/run_settings.ts
@@ -1,5 +1,5 @@
 export const microDebugging:string[] = [] 
-export const SLACK_ON = false;
+export const SLACK_ON = true;
 export const isDeploy = process.env[`IS_DEPLOY`] === `true`;
 export const delta = 0.1 // The threshold we use for when to report a change
 export const channelId = isDeploy ? "C069HTSPS69" : "C06ACLAUTDE";
