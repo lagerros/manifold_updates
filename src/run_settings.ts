80b91a2eb432f9590e536885c4dc10bb66fe92b6
lagerros <jacobklagerros@gmail.com>
2023-12-21 08:41:28 +0000

    always message slack on deploy

--- src/run_settings.ts
+++ src/run_settings.ts
@@ -1,8 +1,19 @@
 export const microDebugging:string[] = ["https://manifold.markets/JacobJacob/which-risk-categories-and-concepts"] 
-export const SLACK_ON = true;
 export const isDeploy = process.env[`IS_DEPLOY`] === `true`;
+
+
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+// User settings
+export const SLACK_ON = isDeploy || true;
 export const delta = 0.1 // The threshold we use for when to report a change
+export const prodChannelId = "C069HTSPS69"
 export const devChannelId = "C06ACLAUTDE"
-export const channelId = isDeploy ? "C069HTSPS69" : devChannelId;
+
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+
+
+export const channelId = isDeploy ? prodChannelId : devChannelId;
 export const mainWebhook = process.env[`SLACK_MAIN_WEBHOOK`]
 export const devWebhook = process.env[`SLACK_DEV_CHANNEL_WEBHOOK`]
