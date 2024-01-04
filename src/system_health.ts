4218b102dc4a7748ffc7ad3e334cc30fc500fd10
lagerros <jacobklagerros@gmail.com>
2024-01-04 16:55:19 +0000

    tiny fix

--- src/system_health.ts
+++ src/system_health.ts
@@ -5,26 +5,26 @@ export const listenAndSendErrorsToSlack = async () => {
   process.on('uncaughtException', (error) => {
     console.error('Uncaught Exception:', error);
     if (devWebhook) {
-      sendDevSlackUpdate(devWebhook, {channelId: devAlertsChannelId, message: `Error in program. Uncaught Exception at: ${error}`})
+      sendDevSlackUpdate(devWebhook, { channelId: devAlertsChannelId, message: `Error in program. Uncaught Exception at: ${error}` })
     }
   });
 
   process.on('unhandledRejection', (reason, promise) => {
     console.error('Unhandled Rejection at:', promise, 'reason:', reason);
     if (devWebhook) {
-      sendDevSlackUpdate(devWebhook, {channelId: devAlertsChannelId, message: `Error in program. Unhandled Rejection at: ${promise} reason: ${reason}`})
+      sendDevSlackUpdate(devWebhook, { channelId: devAlertsChannelId, message: `Error in program. Unhandled Rejection at: ${promise} reason: ${reason}` })
     }
   });
 }
-  
+
 export const systemHealthUpdate = async (): Promise<void> => {
   if (devWebhook) {
-    await sendDevSlackUpdate(devWebhook, {channelId: devAlertsChannelId, message: `Bot still running (${isDeploy ? " :tophat: production" : " :computer: dev"}). :white_check_mark:`})
+    await sendDevSlackUpdate(devWebhook, { channelId: devAlertsChannelId, message: `Bot still running (${isDeploy ? " :tophat: production" : " :computer: dev"}). :white_check_mark:` })
   }
 }
 
 export const systemStartUpdate = async (): Promise<void> => {
   if (devWebhook) {
-    await sendDevSlackUpdate(devWebhook, {channelId: devAlertsChannelId, message: `Bot restarted (${isDeploy ? " :tophat: production" : " :computer: dev"})`})
+    await sendDevSlackUpdate(devWebhook, { channelId: devAlertsChannelId, message: `Bot restarted (${isDeploy ? " :tophat: production" : " :computer: dev"})` })
   }
 }
